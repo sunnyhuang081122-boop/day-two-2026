ea7890c07e90213e9556ec9b8ae216b6743fe274
sunnyhuang081122-boop <sunny.huang081122@gmail.com>
2026-01-04 23:48:13 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 export const supabase = createClient(
-  'https://YOUR_PROJECT_ID.supabase.co',
-  'YOUR_PUBLIC_ANON_KEY'
+  'https://asdzewabsbpmskmtsekj.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzZHpld2Fic2JwbXNrbXRzZWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyMTY1ODcsImV4cCI6MjA4MTc5MjU4N30.NOccLhA7E0MUB1ZNDk4f0NjhcV2qSEQx0i058zrZqPE'
 )
